ede410829e906b4d090afc34ecca1600f3c7fe42
Bhujan Sharma <bhujanv@proximity.bbdo.fr>
2015-09-28 15:46:38 +0400

    Add work in progress solution files

---
+++ MessageEncrypter.Tests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MessageEncrypter;
+
+namespace MessageEncrypter.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            char expected = 'b';
+            char msg = 'a';
+            EncryptionHelper th = new EncryptionHelper();
+            
+            //act
+            char actual = th.encryptAlphabet(msg);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string expected = "bcd";
+            string msg = "abc";
+
+            EncryptionHelper th = new EncryptionHelper();
+
+            string actual = th.encryptMessage(msg);
+
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string expected = "bcd";
+            string msg = "555";
+
+            EncryptionHelper th = new EncryptionHelper();
+
+            string actual = th.encryptMessage(msg);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string expected = "aaa";
+            string msg = "zzz";
+
+            EncryptionHelper th = new EncryptionHelper();
+
+            string actual = th.encryptMessage(msg);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ MessageEncrypter/Encrypter.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+
+namespace MessageEncrypter
+{
+    public partial class Encrypter : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+            Response.Cookies["userName"].Value = "patrick";
+            Response.Cookies["userName"].Expires = DateTime.Now.AddDays(1);
+
+            HttpCookie aCookie = new HttpCookie("lastVisit");
+            aCookie.Value = DateTime.Now.ToString();
+            aCookie.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(aCookie);
+        }
+    }
+}
